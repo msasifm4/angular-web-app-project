5b517b52e757ecb8a727c304d5837d448eef6c64
Mohammed Asif Shaikh <msasifm4@gmail.com>
2024-02-29 15:40:19 +0530

    first commit

--- src/app/modules/users/components/movies/movies.component.ts
+++ src/app/modules/users/components/movies/movies.component.ts
@@ -2,8 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Movie } from '../../../movie';
 import { MovieStore } from '../../../../services/moviestore.service';
-import { MovieService } from '../../../../services/movie.service';
-import { Movie2 } from '../../../movie2';
+
+
 
 @Component({
   selector: 'app-movies',
@@ -11,29 +11,13 @@ import { Movie2 } from '../../../movie2';
   styleUrls: ['./movies.component.css']
 })
 export class MoviesComponent implements OnInit {
-  movie: Movie2 | undefined;
-  movies: Movie2[] = [];
+  movie: Movie | undefined;
+ 
   
   
-  constructor(private route: ActivatedRoute, private movieStore: MovieStore, private movieService: MovieService,) { }
+  constructor(private route: ActivatedRoute, private movieStore: MovieStore,) { }
 
   ngOnInit(): void {
-    this.movieService.getMovies().subscribe(
-      (data: any) => {
-        console.log('Received movies:', data.movies); // Log the received movies
-        this.movies = data.movies.map((movie: any) => ({
-          id: movie.id,
-          title: movie.title,
-          releaseYear: parseInt(movie.releaseYear) // Convert releaseYear to a number
-        }));
-        this.movieStore.setMovies(this.movies);
-      },
-      (error) => {
-        console.error('Error fetching movies:', error);
-      }
-    );
-
-
     this.route.params.subscribe(params => {
       const movieTitle = params['title'];
       this.movie = this.movieStore.getMovieByTitle(movieTitle);
